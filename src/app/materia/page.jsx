9bb38c273498d6690227f654d781751f82714c0b
FranklinRiabani <fmercado@ucb.edu.bo>
2023-11-06 21:12:20 -0400

    rutas de  navegacion

---
+++ src/app/materia/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Materias(){
+    return(
+        <div>
+            <h1>Materias</h1>
+            <Link href="/">Inicio</Link>
+        </div>
+    )
+
+}
