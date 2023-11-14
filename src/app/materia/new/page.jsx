ce0893112d37846649d8a88342be10af03b7081e
FranklinRiabani <fmercado@ucb.edu.bo>
2023-11-14 01:31:52 -0400

    crear

--- src/app/materia/new/page.jsx
+++ src/app/materia/new/page.jsx
@@ -1,22 +1,44 @@
-
-import { useState } from "react"
+"use client"
+import { useEffect, useState } from "react";
+import { useRouter,useParams } from "next/navigation";
 
 function page() {
-    const [materia,setMateria] = useState[
+    const [materia,setMateria] = useState(
         {
             titulo:"",
             imagen:"",
             visible:"",
             orden:""
         }
-    ];
+    );
 
+    const router = useRouter();
+    const params = useParams();
+
+    const handlerSubmit=async(e)=>{
+        e.preventDefault();
+        console.log(materia);
+
+        const res = await fetch('/api/materia',{
+            method:'POST',
+            body:JSON.stringify(materia)
+        })
+
+        const data = await res.json();
+        console.log(data);
+        router.push('/materia');
+        router.refresh();
+    }
+    const handlerChange=(e)=>{
+        //console.log(e.target.value)
+        setMateria({...materia,[e.target.name]:e.target.value})
+    }
   return (
-    <form>
-        <input type="text" name="titulo" />
-        <input type="text" name="imagen" />
-        <input type="text" name="visible" />
-        <input type="text" name="orden" />
+    <form onSubmit={handlerSubmit}>
+        <input type="text" name="titulo" onChange={handlerChange}/>
+        <input type="text" name="imagen" onChange={handlerChange}/>
+        <input type="text" name="visible" onChange={handlerChange}/>
+        <input type="text" name="orden" onChange={handlerChange}/>
         <button type="submit">Registrar Materia</button>
     </form>
   )
--- src/componets/MateriaCard.jsx
+++ src/componets/MateriaCard.jsx
@@ -1,15 +1,15 @@
 import Link from "next/link"
 function MateriaCard({materia}) {
   return (
-        <Link href="/">
-        <div className="bg-gray-700 
-                    p-10 
-                    mt-5 
-                    text-white 
-                    rounded-xl 
-                    hover:bg-gray-500" >
-            <h1>{materia.titulo}</h1>
-        </div>
+        <Link href={`/materia/${materia._id}`}>
+          <div className="bg-gray-700 
+                      p-10 
+                      mt-5 
+                      text-white 
+                      rounded-xl 
+                      hover:bg-gray-500" >
+              <h1>{materia.titulo}</h1>
+          </div>
     </Link>
   )
 }
